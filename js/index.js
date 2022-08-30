98f25cabe4dd0489797a1051d6bfd118630891ad
Wushuoaaa <2832731757@qq.com>
2022-08-30 22:34:31 +0800

    1
    1

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  var ul = document.getElementsByClassName("lunbo1")[0];
+  var liList = ul.children;
+  var liListLength = liList.length;
+  var count = 0;
+  setInterval(() => {
+    if (count + 1 == liListLength) {
+      ul.scrollTop = 0;
+      count = 0;
+    } else {
+      count++;
+      let top = count * 780;
+      var coutBanner = 0;
+
+    //   for (let i = (count - 1) * 780 + 10; i <= top; i++) {
+    //     coutBanner++;
+    //     setTimeout(() => {
+    //       ul.scrollTop = i;
+    //     }, coutBanner * 10);
+    //   }
+      ul.scrollTop = top;
+    }
+  }, 1000);
+};
